d68ec3e71f9a1d3d53260a01f5a1e52fa58e15f3
Lewis Daly <lewisdaly@me.com>
2015-11-14 16:45:20 +1030

    test

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-// var express = require('express');
+var express = require('express');
 
-// var server = express();
-// server.use(express.static(__dirname + '/ionic_rainapp/www'));
+var server = express();
+server.use(express.static(__dirname + '/ionic_rainapp/www'));
 
-// var port = 80;
+server.get('/test', function(req, res){
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+  	res.end('Hello World\n');
+});
+
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
 // server.listen(port, function() {
 // 	console.log("server listening on port " + port);
 // });
 
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+
+
+// var http = require('http')
+// var port = process.env.PORT || 1337;
+// http.createServer(function(req, res) {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end('Hello World\n');
+// }).listen(port);
