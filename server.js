762e9226de7f2ccbdef7838a846d6a6a2efece86
Lewis Daly <lewisdaly@me.com>
2015-11-14 16:37:07 +1030

    removed express

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-var express = require('express');
+// var express = require('express');
 
-var server = express();
-server.use(express.static(__dirname + '/ionic_rainapp/www'));
+// var server = express();
+// server.use(express.static(__dirname + '/ionic_rainapp/www'));
 
-var port = 80;
-server.listen(port, function() {
-	console.log("server listening on port " + port);
-});
+// var port = 80;
+// server.listen(port, function() {
+// 	console.log("server listening on port " + port);
+// });
+
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
