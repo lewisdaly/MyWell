a432ec41e6c9b2fbb8e8116f4f60ba5da8b148e0
Lewis Daly <lewisdaly@me.com>
2016-01-23 14:01:14 +1030

    Styling changes

--- ionic_rainapp/www/js/controller/settings_ctrl.js
+++ ionic_rainapp/www/js/controller/settings_ctrl.js
@@ -3,30 +3,14 @@ angular.module('controller.settings', ['ionic'])
 .controller('SettingsCtrl', ['$scope', 'AuthenticationService', '$location', '$rootScope', 'Azureservice', '$ionicModal', 
 	function($scope, AuthenticationService, $location, $rootScope, Azureservice, $ionicModal) {
 
-
 		$scope.$on('$ionicView.enter', function(e) {
-		//if the user is an admin, load the list of requesting users!
-		$scope.unverifiedUsers = [];
-		$scope.isVerified = false;
+			checkUserStatus();
+			
+		});
 
-
-		var currentUser = $rootScope.globals.currentUser;
-		// console.log("Settings: currentUser: " + JSON.stringify(currentUser));
-
-		if (currentUser) {
-			if (currentUser.verified == true) {
-				$scope.isVerified = true;
-				console.log("Requesting unverified users!");
-				Azureservice.invokeApi("authenticateuser", {method:'get'})
-				.then(function(response) {
-					$scope.unverifiedUsers = response;
-				},
-				function(error) {
-					console.log("Error: " + JSON.stringify(error));
-				})
-			}
-		}
-	});
+		$scope.$on('login-state-changed', function(e) {
+    		checkUserStatus();
+  		});
 
 		$scope.logout = function() {
 			AuthenticationService.ClearCredentials();
@@ -35,17 +19,53 @@ angular.module('controller.settings', ['ionic'])
 			$scope.unverifiedUsers = [];
 		}
 
+		function checkUserStatus() {
+
+			//if the user is an admin, load the list of requesting users!
+			$scope.unverifiedUsers = [];
+			$scope.isUserNotLoggedIn = false;
+			$scope.isUserNotVerified = false;
+			$scope.isUserLoggedInAndVerified = false;
+			$scope.isVerified = false;
+
+			var currentUser = $rootScope.globals.currentUser;
+
+	    	if (!currentUser) {
+				$scope.isUserNotLoggedIn = true;
+				}
+			else if (currentUser.verified == false) {
+			    $scope.isUserNotVerified = true;
+		    }
+		    else {
+		    	$scope.isUserLoggedInAndVerified = true;
+				$scope.isVerified = true;
+				loadUnverifiedUsers();
+			}
+		}
+
+
+		function loadUnverifiedUsers() {
+			console.log("Requesting unverified users!");
+			Azureservice.invokeApi("authenticateuser", {method:'get'})
+			.then(function(response) {
+				$scope.unverifiedUsers = response;
+			},
+			function(error) {
+				console.log("Error: " + JSON.stringify(error));
+			})
+		}
+
+
+		/**
+		 * Modal Methods
+		 */
+
 		$ionicModal.fromTemplateUrl('templates/new-well.html', {
 			scope: $scope,
 			animation: 'slide-in-up'
 		}).then(function(modal) {
 			$scope.modal = modal
 		})  
-
-
-		/**
-		 * Modal Methods
-		 */
 
 		$scope.register = function() {
 			$scope.modal.show()
@@ -61,8 +81,6 @@ angular.module('controller.settings', ['ionic'])
 			//var params = $scope.form
 			//params = { well_owner, well_id, postcode, max_wt_depth, lat, lng}
 			//TODO: verify and submit
-
-
 
 			console.log("Data: " + center);
 			if (center != null) {
@@ -86,17 +104,23 @@ angular.module('controller.settings', ['ionic'])
 
 		});
 
+
+		/**
+		 * Verify a user
+		 */
+
 		$scope.verify = function(userID) {
 			console.log("Verify");
 			var data = {"user_id" : userID, "verified" : true};
 			Azureservice.invokeApi("authenticateuser",{method:'put', body:data})
 			.then(function(response) {
-				console.log("Success! " + JSON.stringify(response))
+				console.log("Success! " + JSON.stringify(response));
+				loadUnverifiedUsers(); 
+
 			},
 			function(error) {
 				console.log("Error: " + JSON.stringify(error));
 			})
 		}
 
-
-	}])
+}])
